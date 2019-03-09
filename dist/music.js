cbec989c3a89345e5ca079076157a2e33426bf99
Yeasion <Yeasion@outlook.com>
2019-03-09 21:33:39 +0800

    Site updated: 2019-03-09 21:33:21

--- dist/music.js
+++ dist/music.js
@@ -16,10 +16,34 @@ const ap = new APlayer({
         cover: 'https://pic.superbed.cn/item/5c660aeb5f3e509ed9781e46',
       },
       {
-        name: 'ブルーバード(青鳥)',
+        name: 'ブルーバード[青鳥]',
         artist: '生物股長',
         url: 'http://www.ytmp3.cn/down/36498.mp3',
         cover: 'https://ww1.sinaimg.cn/large/007i4MEmgy1g06tgdy957j30zk0k0q5a.jpg',
+      },
+      {
+        name: 'アイロニ-[反语]',
+        artist: '4円',
+        url: 'http://www.ytmp3.cn/down/51036.mp3',
+        cover: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1552107089869&di=6937d2da568735e7d256dae02759a11d&imgtype=0&src=http%3A%2F%2Fp1.music.126.net%2FiFKN61k9tOCRoq8hg9ri3Q%3D%3D%2F109951163523069337.jpg',
+      },
+      {
+        name: '暁の鎮魂歌',
+        artist: 'Linked Horizon',
+        url: 'http://win.web.nf01.sycdn.kuwo.cn/568890173cf72c88b976fd7b3450ef3d/5c831fa2/resource/n1/47/60/3734609011.mp3',
+        cover: 'https://y.gtimg.cn/music/photo_new/T002R300x300M00000312jqw2AxkhU.jpg?max_age=2592000',
+      },
+      {
+        name: '白雪 ~sirayuki~',
+        artist: 'とのん',
+        url: 'http://www.ytmp3.cn/down/36120.mp3',
+        cover: 'https://pic.xiami.net/images/album/img0/62500/4055451286766365.jpg?x-oss-process=image/resize,limit_0,s_410,m_fill',
+      },
+      {
+        name: 'Genius',
+        artist: 'LSD',
+        url: 'http://www.ydj8.com/url/qq.php/213870506.mp3',
+        cover: 'https://ss1.baidu.com/6ONXsjip0QIZ8tyhnq/it/u=2733699664,1627965535&fm=58&bpow=1500&bpoh=1500',
       }
     ]
 });
